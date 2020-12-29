f9d125cba341e2d0e6e3444436e5f61c32ce035a
Ola Toft <olatoft@hotmail.com>
2020-12-29 17:55:29 +0100

    Add task to parent. (#5)
    API: Add function to create new task from title and add it to parent.
    CLI: Get title from user input. Use API to add task to parent.

--- Program.fs
+++ Program.fs
@@ -24,9 +24,50 @@ and Task = {
     Children: Task list
 }
 
+module Api =
+    let addTask parent title =
+        let task = {
+            Title = title
+            Status = Todo
+            Parent = parent
+            Children = []
+        }
+
+        let newChildren =
+            match parent with
+            | Task parent -> task :: parent.Children
+            | Root parent -> task :: parent.Children
+
+        let newParent =
+            match parent with
+            | Task parent -> Parent.Task { parent with Children = newChildren }
+            | Root parent -> Parent.Root { parent with Children = newChildren }
+
+        newParent
+
 // -------------
 // ---- CLI ----
 // -------------
+
+module Cli =
+    let getUserInput message =
+        printf message
+        let input = System.Console.ReadLine()
+        input
+
+    let getTitle () =
+        let title = getUserInput "Tittel: "
+        Title title
+
+    let addTask parent =
+        let title = getTitle ()
+        Api.addTask parent title
+
+let root = Parent.Root {
+    Children = []
+}
+
+let newRoot = Cli.addTask root
 
 [<EntryPoint>]
 let main _ =
